fefc626a71141f973423381ca157d781c83801b6
Василий Чинарев <vasa.chi@gmail.com>
2012-07-30 17:25:16 +0400

    Первая рабочая версия

---
+++ imager.rb
@@ -0,0 +1,12 @@
+require "chunky_png"
+require "sinatra"
+
+configure do
+  mime_type :png, 'image/png'
+end
+
+get %r{^/image/(\d+)x(\d+)$} do
+  png = ChunkyPNG::Image.new(params[:captures][0].to_i, params[:captures][1].to_i, ChunkyPNG::Color.rgb(128, 128, 128))
+  content_type :png
+  png.to_blob
+end
