c703739c3fc8f7590b51532fb004724928fe77e2
Василий Чинарев <vasa.chi@gmail.com>
2012-07-30 18:46:02 +0400

    Добавил настройки цвета

--- imager.rb
+++ imager.rb
@@ -5,8 +5,25 @@ configure do
   mime_type :png, 'image/png'
 end
 
-get %r{^/image/(\d+)x(\d+)$} do
-  png = ChunkyPNG::Image.new(params[:captures][0].to_i, params[:captures][1].to_i, ChunkyPNG::Color.rgb(128, 128, 128))
+get %r{^/image/(\d+)x(\d+)(x[a-f0-9]{3}[a-f0-9]{3}?)?\.png$} do
+
+  #получаем обязательные размеры изображения
+  width, height = params[:captures][0].to_i, params[:captures][1].to_i
+
+  #получаем цвет. Может быть задан в виде #abc или #aabbcc. Если цвет не задан, то выставляется серый.
+  color         = params[:captures][2]
+  if color.nil?
+    color = [128, 128, 128]
+  else
+    color     = color[1..-1]
+    span, mul = color.size == 3 ? [1, 2] : [2, 1]
+    color     = color.scan(/.{#{span}}/).map do |c|
+      (c*mul).to_i(16)
+    end
+  end
+
+
+  png = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color.rgb(*color))
   content_type :png
   png.to_blob
 end
